0ccd5a71a8897a1cbf3c625c8fc2dad7e71d6533
janakikrishnakumar <januraja3@gmail.com>
2024-08-05 16:55:13 +0530

    commit1

---
+++ src/test/java/org_ST/CreateOrg_ST.java
@@ -0,0 +1,27 @@
+package org_ST;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class CreateOrg_ST {
+	@Test
+	public void nameOrg() {
+		String expectedpage="Home";
+		WebDriver driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.manage().window().maximize();
+		driver.get("http://localhost:8888/");
+		
+		
+		
+		
+		driver.findElement(By.name("user_name")).sendKeys("admin");
+		driver.findElement(By.name("user_password")).sendKeys("admin");
+		driver.findElement(By.id("submitButton")).click();
+		String title=driver.findElement(By.xpath("//a[contains (text(),'Home')]")).getText();
+	}
+}
